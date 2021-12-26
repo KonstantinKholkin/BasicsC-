e86a1e8a9b135ab4d2b597be216049b417f1da89
KHKonstantin <Konstantinkholkin@gmail.com>
2021-12-26 20:51:43 +0300

    Completed Lessons4

---
+++ Lessons4/Exercise4/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+   Задача 4
+   Решить задачу с логинами из урока 2, только логины и пароли считать из файла в массив. Создайте структуру Account, содержащую Login и Password.
+
+
+
+Выполнил Холкин Константин Юрьевич
+*/
+namespace Exercise4
+{
+
+
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            AccountUsing my = new AccountUsing();
+            my.FileSave();
+            string accountFile = @"E:\GeekBrains\BasicsC#\Lessons4\Exercise4\account.txt";
+            my.ReadArrayFromFile(accountFile);
+            my.GetLoginPass();
+        }
+
+    }
+}
+
+
+
+
+
+   
---
+++ Lessons4/Exercise5/Array2.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+   Задача 5
+   *а) Реализовать библиотеку с классом для работы с двумерным массивом. Реализовать конструктор, заполняющий массив случайными числами.
+   Создать методы, которые возвращают сумму всех элементов массива, сумму всех элементов массива больше заданного, свойство,
+   возвращающее минимальный элемент массива, свойство, возвращающее максимальный элемент массива,
+   метод, возвращающий номер максимального элемента массива (через параметры, используя модификатор ref или out).
+   **б) Добавить конструктор и методы, которые загружают данные из файла и записывают данные в файл.
+   **в) Обработать возможные исключительные ситуации при работе с файлами.
+
+
+
+
+Выполнил Холкин Константин Юрьевич
+*/
+
+namespace Exercise5
+{
+    internal class Array2
+    {
+        int[,] array = new int[2,2];//Создаем массив
+        Random random = new Random();
+
+        public void Array()//Заполняем массив
+        { 
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(10);
+                }
+            }
+          
+        }
+        public void PrintArray()//Выводим массмв
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                   Console.Write($"  {array[i, j]}  " );
+                }
+                Console.Write("\n");
+            }
+
+        }
+        public void Summ()//Сумма элементов массива
+        {
+            int sum = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    sum += array[i, j];
+                }
+                
+            }
+            Console.WriteLine($"\n Сумма всех чисел в массиве равна = {sum}");
+
+        }
+
+        public void SumElementss()//Число Элементов массива
+        {
+            int sum = 0;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    sum++;
+                }
+
+            }
+            Console.WriteLine($"\n Сумма элементов массива равна = {sum}");
+
+        }
+        public int MaxCounter()// Вычисляем максимальное число
+        {
+            int max = array[0, 0];
+            
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] > 0 && array[i, j] >max)
+                        max = array[i, j];
+                    
+                }
+            }
+
+            Console.WriteLine($"\n\nMAX число {max}\n\n");
+            return max;
+        }
+        public int MinCounter()// Вычисляем минимально число
+        {
+            int min = array[0, 0];
+           
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] > 0 && array[i, j] < min)
+                        min = array[i, j];
+
+                }
+            }
+
+            Console.WriteLine($"\n\nMIN число {min}\n\n");
+            return min;
+        }
+        public int Max()// Вычисляем сумму чисел больше задонного
+        {
+            int c = 0;
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] > 0 && array[i, j] > 2)
+                        c++;
+
+                }
+            }
+
+            Console.WriteLine($"\n\nСумма чисел больше заданного {c}\n\n");
+            return c;
+        }
+    }
+}
