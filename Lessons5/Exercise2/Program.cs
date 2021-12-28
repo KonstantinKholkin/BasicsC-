ea71d4ad039edab7d4fca8d3b2249d37645a8a45
KHKonstantin <Konstantinkholkin@gmail.com>
2021-12-28 20:56:59 +0300

    Created Lessons5

---
+++ Lessons5/Exercise2/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Exercise2;
+Console.WriteLine("\nДанн стих: \n" + ResultText.text);
+
+Console.WriteLine("\nВыведем слова текста, которые содержат не более 6 букв:\n");
+ResultText.GetWordsByLength(6);
+
+
+Console.Write("\nУдалим из текста слова, заканчивающиеся на 'и': \n");
+ResultText.DeleteWordByEndChar('и');
+
+Console.WriteLine("\nСамое длинное слово в тексте: " + ResultText.FindMaxLengthWord());
+
+Console.WriteLine("\nПроизведём частотный анализ текста: \n");
+string[] arr = { "Новый", "год", "нам", "подарки" };
+ResultText.FrequencyAnalysis(arr, ResultText.text);
+
+
---
+++ Lessons5/Exercise2/ResultText.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Exercise2
+{
+    
+     class ResultText
+    {
+        static public string text;
+
+       
+        static ResultText()
+        {
+            text = "Новый Год! Новый Год!\nКаждый, кого-то, в гости ждет!\nЖдут детишки, ждут зайчишки\nЖдет и маленький братишка!\n" +
+                   "Сколько ж, к нам придет народу ?\nСобрались бы все по ходу...\nЧтобы Дедушка Мороз,\nНам подарки бы принес!\n" +
+                   "Чтобы, елки на макушках\nЗасияли бы в игрушках...\nЧтобы, каждому под ней\nНабралось по больше пней!\n" +
+                   " Этот пень, зовется, -Книжка\nПро смышленого мальчишку\nБабушка ее купила\nИ в придачу, -кусок мыла.\n" +
+                   "Этот пень, зовется, -Зайка,\nОн недавно из лужайки...\nДедушка его...";
+        }
+
+        
+       
+        static public void GetWordsByLength(int len)//Выводит слова сообщения, которые содержат не более n букв
+        {
+            string[] words = text.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t'});
+            Console.WriteLine($"Вывод слов, длинной не более {len} :\n ");
+            foreach (string word in words)
+            {
+                if (word == "")
+                    continue;
+                if (word.Length <= len)
+                    Console.Write($"{word}   ");
+            }
+        }
+
+        
+        
+        static public void DeleteWordByEndChar(char ch)//Удаляет из сообщения все слова, которые заканчиваются на заданный символ
+        {
+            string[] words = text.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t' });
+            Console.WriteLine($"Будут удалены слова, оканчивающиеся на символ {ch} +:\n ");
+            foreach (string word in words)
+            {
+                if (word == "")
+                    continue;
+                if (word[word.Length - 1] == ch)
+                {
+                    Console.Write($"{word}  ");
+                    text.Replace(word, " ");
+                }
+                
+            }
+            Console.WriteLine($"\nВ результате работы метода, исходный текст изменился на: \n {text}"  );
+        }
+
+       
+        
+        static public string FindMaxLengthWord() //Находит самое длинное слово сообщения
+        {
+            string[] words = text.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t' });
+            string maxWord = words[0];
+            int max = words[0].Length;
+            
+            foreach (string word in words)
+            {
+                if (word.Length > max)
+                {
+                    max = word.Length;
+                    maxWord = word;
+                }
+            }
+            Console.WriteLine($"Слово с самой большой длинной:\n{maxWord}" );
+            return maxWord;
+        }
+
+              
+       
+       
+        static  public void FrequencyAnalysis(string[] words, string text) //Производит частотный анализ текста
+        {
+            Dictionary<string, int> wordFrequency = new Dictionary<string, int>();
+
+            string[] textWords = text.Split(new Char[] { ' ', ',', '.', '-', '\n', '\t' });
+            foreach (string word in words)
+            {
+                foreach (string wordInText in textWords)
+                {
+                    if (word == "")
+                        continue;
+                    if (wordInText == word)
+                    {
+                        if (word == "")
+                            continue;
+                        if (wordFrequency.ContainsKey(word))
+                            wordFrequency[word] += 1;
+                        else
+                            wordFrequency.Add(word, 1);
+                    }
+                }
+            }
+            Console.WriteLine("Частотный анализ текста дал следующий результат:\n ");
+            ICollection<string> keys = wordFrequency.Keys;
+
+            String result = String.Format("{0,-10} {1,-10}\n\n", "Слово", "Частота появления");
+
+            foreach (string key in keys)
+                result += String.Format("{0,-10} {1,-10:N0}\n",
+                                   key, wordFrequency[key]);
+            Console.WriteLine($"\n{result}");
+        }
+    }
+}
