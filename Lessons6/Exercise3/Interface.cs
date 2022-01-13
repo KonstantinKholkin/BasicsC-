d8f9b501027b784b9fc679301f8c4995b59c635c
KHKonstantin <Konstantinkholkin@gmail.com>
2022-01-14 01:55:32 +0300

    Completed ex.3

---
+++ Lessons6/Exercise3/Interface.cs
@@ -0,0 +1,102 @@
+﻿
+
+/*
+ Задание 3:
+Переделать программу Пример использования коллекций для решения следующих задач:
+а) Подсчитать количество студентов учащихся на 5 и 6 курсах;
+б) подсчитать сколько студентов в возрасте от 18 до 20 лет на каком курсе учатся (*частотный массив);
+в) отсортировать список по возрасту студента;
+г) *отсортировать список по курсу и возрасту студента;
+
+
+
+   Выполнил: Холкин Константин Юрьевич
+*/
+namespace Exercise3
+{
+    class Interface
+    {
+        int bakalavr = 0;
+        int magistr = 0;
+        public List<Student> list = new List<Student>();// Создаем список студентов
+            DateTime dt = DateTime.Now;
+            public Dictionary<int, int> cousreFrequency = new Dictionary<int, int>();
+            StreamReader sr = new StreamReader("students_6.csv");
+        static int MyDelegat(Student st1, Student st2)          // Создаем метод для сравнения для экземпляров
+        {
+            return String.Compare(st1.age.ToString(), st2.age.ToString());          // Сравниваем две строки
+        }
+
+        static int CourceAndAgeCompare(Student st1, Student st2)
+        {
+            if (st1.course > st2.course)
+                return 1;
+            if (st1.course < st2.course)
+                return -1;
+            if (st1.age > st2.age)
+                return 1;
+            if (st1.age < st2.age)
+                return -1;
+            return 0;
+        }
+            
+
+        public void StudentCalcul()
+        {
+            
+            while (!sr.EndOfStream)
+            {
+                try
+                {
+                    string[] s = sr.ReadLine().Split(';');
+
+                    list.Add(new Student(s[0], s[1], s[2], s[3], s[4], int.Parse(s[5]), int.Parse(s[6]), int.Parse(s[7]), s[8]));// Добавляем в список новый экземпляр класса Student
+
+                    if (int.Parse(s[5]) < 5) bakalavr++; else magistr++; // Одновременно подсчитываем количество бакалавров и магистров
+
+                    if (int.Parse(s[6]) > 17 && int.Parse(s[6]) < 21)
+                    {
+                        if (cousreFrequency.ContainsKey(int.Parse(s[6])))
+                            cousreFrequency[int.Parse(s[6])] += 1;
+                        else
+                            cousreFrequency.Add(int.Parse(s[6]), 1);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine("Ошибка!ESC - прекратить выполнение программы");
+                    // Выход из Main
+                    if (Console.ReadKey().Key == ConsoleKey.Escape) return;
+                }
+            }
+            sr.Close();
+            list.Sort(new Comparison<Student>(MyDelegat));
+            Console.WriteLine("Всего студентов:" + list.Count);
+            Console.WriteLine("Магистров:{0}", magistr);
+            Console.WriteLine("Бакалавров:{0}", bakalavr);
+
+            Console.WriteLine("\nСтуденты в возрасте от 18 до 20 лет и на каком курсе учатся.");
+            ICollection<int> keys = cousreFrequency.Keys;
+            String result = String.Format("{0,-10} {1,-10}\n", "Возраст", "Количество студентов");
+            foreach (int key in keys)
+                result += String.Format("{0,-10} {1,-10:N0}\n",
+                                   key, cousreFrequency[key]);
+            Console.WriteLine($"\n{result}");
+
+            list.Sort(new Comparison<Student>(MyDelegat));
+            Console.WriteLine("Сортируем студентов по возрасту: ");
+            foreach (var v in list) Console.WriteLine($"{v.firstName} {v.age}");
+
+            list.Sort(new Comparison<Student>(CourceAndAgeCompare));
+            Console.WriteLine("\nСортируем студентов по курсу и возрасту возрасту: ");
+            foreach (var v in list) Console.WriteLine($"{v.firstName}, курс {v.course}, возраст {v.age}");
+
+            Console.WriteLine(DateTime.Now - dt);
+            Console.ReadKey();
+
+            
+            
+        }
+    }
+}
